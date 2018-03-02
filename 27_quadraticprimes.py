0f0ad7301bac5bf6a31f54cab68b2e4c531c7519
jrialland <julien.rialland@gmail.com>
2018-03-02 15:05:18 +0100

    separate file for reused fns

--- 27_quadraticprimes.py
+++ 27_quadraticprimes.py
@@ -1,19 +1,5 @@
 
-_primes=set([])
-def is_prime(n):
-  if n in _primes: return True
-  if n == 2 or n == 3: return True
-  if n < 2 or n%2 == 0: return False
-  if n < 9: return True
-  if n%3 == 0: return False
-  r = int(n**0.5)
-  f = 5
-  while f <= r:
-    if n%f == 0: return False
-    if n%(f+2) == 0: return False
-    f +=6
-  _primes.add(n)
-  return True
+from utils import is_prime
 
 def count_quadprimes(a, b):
     n = 1
@@ -21,13 +7,12 @@ def count_quadprimes(a, b):
        n+=1
     return n
 
-#----------------
-maxprimes = 0
-for a in range(-999, 1000):
+m, sa, sb = 0, 0, 0
+for a in range(-999,1000):
     for b in range(-999, 1000):
-        c = count_quadprimes(a, b)
-        if c > maxprimes:
-            maxprimes = c
-            solution = a * b
-print solution
+       c = count_quadprimes(a,b)
+       if c > m:
+           m = c
+           sa, sb = a, b
+print sa * sb
 
